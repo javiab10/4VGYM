d6e8ddc8ea02dd686322fea39201eb54ba7ddcb9
javiab10 <javierbasoko@gmail.com>
2024-12-08 23:28:29 +0100

    Creación de una tabla "sólida"
    Hemos creado una tabla como un componente de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MenuComponent } from './menu/menu.component';
 import { ActivityComponent } from "./activity/activity.component";
 import { ListComponent } from "./list/list.component";
+import { TablaComponent } from './tabla/tabla.component';
 
 @Component({
   selector: 'app-root',
-  imports: [MenuComponent, ActivityComponent, ListComponent], //Nombre de la clase a importar
+  imports: [MenuComponent, ActivityComponent, ListComponent, TablaComponent], //Nombre de la clase a importar
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
